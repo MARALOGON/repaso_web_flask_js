1354efa347c2d08a9e9d70811cf7681a78fc1b30
Marcos Alonso <marcosalonso@MacBook-Pro-de-Marcos.local>
2021-06-20 22:53:17 +0200

    csrf_token y algo de css

--- kakebo/kakebo/forms.py
+++ kakebo/kakebo/forms.py
@@ -5,11 +5,11 @@ from datetime import date
 
 def fecha_por_debajo_de_hoy(formulario, campo):  #Aqui creamos una función que nos va a calidadr que en el formulario solo se puedan meter fechas por debajo  de la actual. Le pasamos los parametros formulario y campo, habrá que informar de ambos en la variable fecha de la siguiente clase MovimientosForm
     hoy = date.today
-    if campo.data >hoy:
+    if campo.data > hoy:
         raise ValidationError('La fecha no puede ser mayor que hoy') #ValidationError es un error especifico de WTForms, por eso hay que importarlo también.
 
 class MovimientosForm(FlaskForm): #Creamos la clase MovimientosForm que hereda del objeto FlaskForm. El objeto FlaskForm automatiza entre otras cosas la validacion de errores de los formularios. En las siguientes lineas se definen los campos y se mete si son requeridos o no 
-    fecha = DateField("Fecha", validators=[DataRequired(message = "Debe informar una fecha valida", fecha_por_debajo_de_hoy)])
+    fecha = DateField("Fecha", validators=[DataRequired(message = "Debe informar una fecha valida"), fecha_por_debajo_de_hoy])
     concepto = StringField("Concepto", validators = [DataRequired(), Length(min=10)])
     categoria = SelectField("Categoria", choices=[('00', ''), ('SU', 'Supervivencia'), ('OV', 'Ocio/Vicio'), ('CU', 'Cultura'), ('EX', 'Extras')])
     cantidad = FloatField("Cantidad", validators = [DataRequired()])
